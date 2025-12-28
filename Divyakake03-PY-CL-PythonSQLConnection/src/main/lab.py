e42f87098da6f5832a9e065e5a160108b6d1adf9
Divyakake03 <divyakake03@gmail.com>
2025-12-28 17:03:35 +0530

    Added Divyakake03-PY-CL-PythonSQLConnection lab files as separate folder

---
+++ Divyakake03-PY-CL-PythonSQLConnection/src/main/lab.py
@@ -0,0 +1,18 @@
+"""
+This lab will explore establishing a database connection via Python and SQLite.
+"""
+import sqlite3
+
+
+class Lab:
+
+    # TODO: Complete the try block in connect_to_database(), returning a Database Connection object
+    def connect_to_database(self):
+
+        try:
+            print("Connecting to database...")
+            connection=sqlite3.connect("example.db")
+            return connection
+        except Exception as e:
+            print(f"Failed to connect to database, with Exception: {e}")
+            return None
